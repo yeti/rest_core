08f15eca2aa357a6a376136f55eb90a082ee9247
Baylee Feore <baylee@yetihq.com>
2014-10-27 16:14:46 -0700

    Bug fix for IsOwner permission

--- rest_core/permissions.py
+++ rest_core/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 
-class IsOwner(permissions.BasePermission):
+class IsOwner(permissions.IsAuthenticated):
     """
     Only the object's owner can view or edit
     Assumes the model instance has a `user` attribute.
